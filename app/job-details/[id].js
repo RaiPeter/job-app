81b66a270df0ef6bc041585bf7a17f13a819aa53
Peter Rai <peter.r@smarter.codes>
2024-01-02 00:41:27 +0530

    updating job details tabs

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,7 +1,7 @@
 import {
     Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl
 } from 'react-native';
-import { Stack, useRouter,useSearchParams } from 'expo-router';
+import { Stack, useRouter,useGlobalSearchParams } from 'expo-router';
 import { useCallback, useState } from 'react';
 
 import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics
@@ -9,8 +9,10 @@ import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics
 import { COLORS, SIZES, icons } from '../../constants';
 import useFetch from '../../hook/useFetch';
 
+const tabs = ["About","Qualifications","Responsibilities"]
+
 const JobDetails = () => {
-    const params = useSearchParams();
+    const params = useGlobalSearchParams();
     const router = useRouter();
 
     const {data, isLoading, error, reFetch} = useFetch("job-details",{
@@ -18,9 +20,26 @@ const JobDetails = () => {
     })
 
     const [refreshing, setRefreshing] = useState(false);
+    const [activeTab, setActiveTab] = useState(tabs[0])
 
-    const onRefresh = ()=> {
+    const onRefresh = useCallback(()=> {
+        setRefreshing(true);
+        reFetch()
+        setRefreshing(false)
+    },[])
 
+    const displayTabContent = () => {
+        switch (activeTab) {
+            case "Qualifications":
+                return <Specifics 
+                    title="Qualifications"
+                    points={data[0].job_highlights?.qualifications ?? ['N/A']}
+                    />
+            case "About":
+            case "Responsibilities":
+            default:
+                break;
+        }
     }
 
     return (
@@ -65,8 +84,12 @@ const JobDetails = () => {
                         />
 
                         <JobTabs
-                        
+                            tabs={tabs}
+                            activeTab={activeTab}
+                            setActiveTab={setActiveTab}
                         />
+
+                        {displayTabContent()}
                     </View>
                 )
                 }
